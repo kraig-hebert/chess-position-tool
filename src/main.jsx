aa938df37d2b3a99134b0b7d52cabb6868a252d1
kraig <kraighebert@gmail.com>
2025-05-05 18:12:47 -0400

    updated store naming

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { GameStateProvider } from "./context/GameStateProvider";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { store } from "./store/store.js";
 
 import App from "./App";
 import "./global.css";
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import gameReducer from "./slices/gameSlice";
+import uiReducer from "./slices/uiSlice";
+import userReducer from "./slices/userSlice";
+import studyReducer from "./slices/studySlice";
+import analysisReducer from "./slices/analysisSlice";
 
 export const store = configureStore({
   reducer: {
     game: gameReducer,
+    ui: uiReducer,
+    user: userReducer,
+    study: studyReducer,
+    analysis: analysisReducer,
   },
 });
